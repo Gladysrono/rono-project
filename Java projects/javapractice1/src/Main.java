bae9686d12c9e306dbbbb98056667ac35f5336b4
Gladysrono <gladyschepkoech408@gmail.com>
2024-05-27 07:18:38 -0700

    updated the java practice projects to reposistory

---
+++ Java projects/javapractice1/src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter your name");
+        int age = Integer.parseInt(JOptionPane.showInputDialog("enter your age"));
+    }
+}
+
